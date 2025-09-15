587815e6e67c655cbd7486b9c261f243c6ad6062
unknown <lienardbaniqued@gmail.com>
2025-09-15 20:50:36 +0800

    Add my project files

---
+++ Assignment4/Age Group.R
@@ -0,0 +1,10 @@
+Age <- 65
+if (Age < 13){
+  print("Child")
+}else if (Age >=13 && Age <=19){
+  print("Teenager")
+}else if (Age >= 20 && Age <= 59){
+  print("Adult")
+}else {
+  print("Senior")
+}
