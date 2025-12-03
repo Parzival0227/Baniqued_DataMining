0e1b08bea0d0d31f5dd2a3424f6f3f4d6450c88e
unknown <lienardbaniqued@gmail.com>
2025-12-03 18:33:48 +0800

    Stop tracking sensitive file

--- Traffic_Lights.R
+++
@@ -1,8 +0,0 @@
-color = "green"
-
-result <- switch(color,
-                 "red" = "Stop",
-                 "yellow" = "Ready",
-                 "green" = "Go", "Invalid Signal"
-)
-print(result)
